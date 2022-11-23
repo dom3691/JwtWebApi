0477b676f60cac2cbb097614f01a34d65b315d29
Dominic James <30402312+dom3691@users.noreply.github.com>
2022-11-23 01:06:56 +0100

    Add project files.

---
+++ JwtWebApi/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register (UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username= request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;   
+            return Ok(user);
+        }
+
+        [HttpPost ("Login")]
+        public async Task <ActionResult<string>> Login (UserDto request)
+        {
+            if (user.Username !=request.Username)
+            {
+                return BadRequest("User Not Found");
+            }
+            return Ok("MY CRAZY TOKEN");
+        }
+
+        private void CreatePasswordHash (string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte );
+    }
+}
