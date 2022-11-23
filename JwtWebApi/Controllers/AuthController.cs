f1ca6cd157abb13e0fd32da22a704d8ad96a6059
Dominic James <30402312+dom3691@users.noreply.github.com>
2022-11-23 13:09:14 +0100

    Third Commit

--- JwtWebApi/Controllers/AuthController.cs
+++ JwtWebApi/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace JwtWebApi.Controllers
 {
@@ -9,6 +13,12 @@ namespace JwtWebApi.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register (UserDto request)
@@ -28,7 +38,37 @@ namespace JwtWebApi.Controllers
             {
                 return BadRequest("User Not Found");
             }
-            return Ok("MY CRAZY TOKEN");
+            if (!VerifyPasswordHash (request.Password, user.PasswordHash, user.PasswordSalt)) 
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim (ClaimTypes.Name, user.Username)
+            };
+            var key = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+              
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+
+
+
+            return string.Empty;
         }
 
         private void CreatePasswordHash (string password, out byte[] passwordHash, out byte[] passwordSalt)
@@ -40,6 +80,13 @@ namespace JwtWebApi.Controllers
             }
         }
 
-        private bool VerifyPasswordHash(string password, byte );
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash (System.Text.Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
     }
 }
